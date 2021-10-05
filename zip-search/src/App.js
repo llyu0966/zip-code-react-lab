c5b8795e75ae02229c70348c77bd31ef2d37f8e9
Liyu Lin <lliyu.brooklyn@gmail.com>
2021-10-05 01:18:30 -0400

    zip code search app

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -2,26 +2,69 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-function City(props) {
-  return (<div>This is the City component</div>);
+function City( {data} ) {
+  return (<div className="City">
+    <div className="City-name">{ data.City }, { data.State }</div>
+    <br></br>
+    <ul>
+      <li>State: { data.State }</li>
+      <li>Location: ({ data.Lat },{ data.Long })</li>
+      <li>Population (estimated): { data.EstimatedPopulation }</li>
+      <li>Total Wages: { data.TotalWages }</li>
+    </ul>
+    </div>);
 }
 
 function ZipSearchField(props) {
-  return (<div>This is the ZipSearchField component</div>);
+  return (
+    <div>
+      <b>Zip Code:</b>
+      <input type="text" onChange={ props.changeHandler }/>
+    </div>
+    );
 }
 
 
 class App extends Component {
+
+  state = {
+    zipCode: '',
+    cities: [],
+  }
+
+  zipChanged = (event) => {
+    
+    console.log(event.target.value);
+    this.setState({ zipCode: event.target.value })
+
+  if(event.target.value.length === 5) {
+    fetch(`http://ctp-zip-api.herokuapp.com/zip/${event.target.value}`)
+     .then((res) => res.json())
+     .then((data) => {
+       console.log(data)
+       this.setState({ cities: data })
+     }) 
+     .catch(err => {
+       console.log(`No results`)
+       this.setState({ cities: [] })
+     })
+    } else {
+      this.setState({ cities: [] })
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        <div className="Result">
+        <ZipSearchField zipCode={this.state.zipCode} changeHandler={this.zipChanged}/>
         <div>
-          <City />
-          <City />
+          { this.state.cities.length === 0 ? <p>No Results</p> : null }
+          { this.state.cities.map((city) => <City data={city}/>)}
+        </div>
         </div>
       </div>
     );
